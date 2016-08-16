291a56a971bd60ca69d480d0caebf5219772db36
Arthur <galiullinko@gmail.com>
2016-08-16 15:49:50 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
+  resources :posts, only: %i(update destroy index show)
+  resources :comments, only: %i(update destroy)
+
+  resources :posts do
+    resources :comments, shallow: true
+  end
+
   root to: "pages#home"
 end
