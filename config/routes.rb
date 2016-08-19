bec886d7ee375ee59c3e9f1519f9a36ef534bd71
Arthur <galiullinko@gmail.com>
2016-08-19 17:13:56 +0300

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
-  resources :posts, only: %i(update destroy index show)
-  resources :comments, only: %i(update destroy)
 
   resources :posts do
     resources :comments, shallow: true
