32861d372c9c37a76999be2a6c1be3703bac9c8d
Arthur <galiullinko@gmail.com>
2016-08-22 23:52:06 +0300

    Added CKEditor

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => "/ckeditor"
   devise_for :users, controllers: { registrations: "users/registrations" }
 
-  resources :posts do
-    resources :comments, shallow: true
+  resources :posts, only: %i(show index) do
+    resources :comments, only: %i(show index)
   end
 
-  root to: "pages#home"
+  namespace :users do
+    resources :posts, only: %i(new create show edit update destroy), shallow: true do
+      resources :comments, only: %i(new show index create destroy)
+    end
+  end
+
+  root to: "posts#index"
 end
