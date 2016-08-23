e92721b65518100db196187676bb15d98e346b96
Arthur <galiullinko@gmail.com>
2016-08-23 17:58:34 +0300

    Changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => "/ckeditor"
   devise_for :users, controllers: { registrations: "users/registrations" }
 
-  resources :posts, only: %i(show index) do
-    resources :comments, only: %i(show index)
-  end
-
   namespace :users do
-    resources :posts, only: %i(new create show edit update destroy), shallow: true do
-      resources :comments, only: %i(new show index create destroy)
+    resources :posts, only: %i(new create edit update destroy) do
+      resources :comments, only: %i(create)
     end
   end
+
+  resources :posts, only: %i(show index)
 
   root to: "posts#index"
 end
