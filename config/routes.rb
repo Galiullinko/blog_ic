8f6173b9fcbe569cf5da5763d2fadb440bf9af50
Arthur <galiullinko@gmail.com>
2016-08-25 14:59:37 +0300

    Changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
 
   namespace :users do
     resources :posts, only: %i(new create edit update destroy) do
-      resources :comments, only: %i(create)
+      resources :comments, only: %i(create destroy)
     end
   end
 
   resources :posts, only: %i(show index)
+
+  get "/users/:user_id/posts/", to: "user_posts#index"
 
   root to: "posts#index"
 end
