d17e9e1482b2d3e389104e25a21d63753d242155
Arthur <galiullinko@gmail.com>
2016-09-08 17:52:16 +0300

    Now delete comment test work properly

--- spec/features/user/comments/delete_comment_spec.rb
+++ spec/features/user/comments/delete_comment_spec.rb
@@ -3,11 +3,10 @@ require "rails_helper"
 feature "Delete comment" do
   include_context "current user signed in"
 
-  let(:post) { create :post }
-  let(:comment) { create :comment, user: current_user, post: post }
+  let!(:post) { create :post }
+  let!(:comment) { create :comment, user: current_user, post: post }
 
   scenario "User deletes own comment" do
-    post.comments = [comment] # TODO: FactoryGirl indexes
     visit post_path(post)
 
     click_on "Delete"
